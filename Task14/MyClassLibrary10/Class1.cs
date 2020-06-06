d824b9057a514abe44a60b81f912e16493feb1be
PestovaAlexandra <63185698+PestovaAlexandra@users.noreply.github.com>
2020-06-06 20:18:46 +0500

    Добавьте файлы проекта.

---
+++ Task14/MyClassLibrary10/Class1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary10
+{
+    public class Person
+    {
+
+        protected string name;
+        protected int age;
+
+        protected static Random rnd = new Random();
+        static string[] NameMale = { "Иван", "Пётр", "Максим", "Андрей", "Сергей", "Данил", "Григорий", "Алексей", "Артём", "Евгений" };
+        static string[] SurnameMale = { "Сидоров", "Петров", "Иванов", "Фёдоровых", "Постнов", "Осипов", "Корзников", "Бутин", "Утробин", "Байбурин" };
+        static string[] NameFemale = { "Жанна", "Светлана", "Елена", "Ольга", "Анжелика", "Юлия", "Анна", "Анастасия", "Ирина", "Александра" };
+        static string[] SurnameFemale = { "Аристова", "Горожанина", "Мазилова", "Бахарева", "Кувыркина", "Пильгун", "Мальцева", "Попова", "Шапикаева", "Ионникова" };
+        static string[] Qualification = { "высшая", "первая", "вторая" };
+        static string MakeName()
+        {
+            if (rnd.Next(0, 2) == 0)
+            {
+                return NameFemale[rnd.Next(NameFemale.Length)] +" "+ SurnameFemale[rnd.Next(SurnameFemale.Length)];
+            }
+            else
+                return NameMale[rnd.Next(NameMale.Length)] +" "+ SurnameMale[rnd.Next(SurnameMale.Length)];
+        }
+        public Person()
+        {
+            name = MakeName();
+            age = rnd.Next(18,99);
+        }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public override string ToString()
+        {
+            return Name.ToString() + " " + Age.ToString();
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {name}\nВозраст: {age}");
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+
+    public class Employee : Person, IComparable, ICloneable,IEquatable<Object>//служащий
+    {
+        protected int experience;//опыт работы
+        public Employee() : base()
+        {
+            experience = rnd.Next(1,70);
+        }
+        public Employee(string name, int age, int experience)
+            : base(name, age)
+        {
+            this.experience = experience;
+        }
+        public int Experience
+        {
+            get { return experience; }
+            set { experience = value; }
+        }
+        public override string ToString()
+        {
+            return base.ToString() +" стаж: " + Experience;
+        }
+        public override void PrintInfo()
+        {
+
+            Console.WriteLine($"Имя: {name}\nВозраст: {age}\nСтаж работы: {experience}");
+        }
+        public int CompareTo(Object obj)//реализация интерфейса
+        {
+            Employee employeeToCompare = (Employee)obj;
+            if (this.experience > employeeToCompare.experience) return 1;
+            if (this.experience < employeeToCompare.experience) return -1;
+            return 0;
+        }
+        public object Clone()
+        {
+            return new Employee("Клон "+ this.name,this.age, this.experience);
+        }
+        public Employee ShallowCopy() //поверхностное копирование
+        {
+            return (Employee)this.MemberwiseClone();
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+        public override bool Equals(Object obj)
+        {
+            try
+            {
+                Employee g = (Employee)obj;
+                return (g.Name == this.Name && g.Age == this.Age && g.Experience == this.Experience);
+            }
+            catch
+            {
+                return false;
+            }
+
+
+        }
+    }
+    public class Worker : Employee//рабочий
+    {
+        private string workPlace;//название предприятия
+        static string[] Factors = { "КамКабель", "БИО Мед", "Завод ГСК", "Лесозавод", "КамГЭС", "Пороховой завод", "НПО Искра", "БумКомбинат", "ПНОС", "ОАО Мотавилихинские заводы" };
+
+        static string MakeFactor()
+        {
+            return Factors[rnd.Next(Factors.Length)];
+        }
+        public Worker() : base()
+        {
+            workPlace =MakeFactor() ;
+        }
+        public Worker(string name, int age, int experience, string workPlace)
+            : base(name,age, experience)
+        {
+            this.workPlace = workPlace;
+        }
+        public string WorkPlace
+        {
+            get { return workPlace; }
+            set { workPlace = value; }
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " место работы: " + WorkPlace ;
+        }
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Место работы: {workPlace}");
+        }
+        public Worker ShallowCopy() //поверхностное копирование
+        {
+            return (Worker)this.MemberwiseClone();
+        }
+        public object Clone()
+        {
+            return new Worker("Клон " + this.name, this.age, this.experience, this.workPlace);
+        }
+        public int CompareTo(object obj)//реализация интерфейса
+        {
+            Worker workerToCompare = (Worker)obj;
+            if (this.experience > workerToCompare.experience) return 1;
+            if (this.experience < workerToCompare.experience) return -1;
+            return 0;
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+    public class Engneer : Employee//инженер
+    {
+        private string qualification;//квалификация
+        static string[] Qualifications = { "высшая", "первая", "вторая" };
+        static string MakeQualification()
+        {
+            return Qualifications[rnd.Next(Qualifications.Length)];
+        }
+        public Engneer() : base()
+        {
+            qualification =MakeQualification() ;
+        }
+        public Engneer(string name, int age, int experience, string qualification)
+            : base(name, age, experience)
+        {
+            this.qualification = qualification;
+        }
+        public string Qualification
+        {
+            get { return qualification; }
+            set { qualification = value; }
+        }
+        public override string ToString()
+        {
+            return base.ToString() + " квалификация: " + Qualification;
+        }
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Квалификация: {qualification}");
+        }
+        public int CompareTo(object obj)//реализация интерфейса
+        {
+            Engneer engeneerToCompare = (Engneer)obj;
+            if (this.experience > engeneerToCompare.experience) return 1;
+            if (this.experience < engeneerToCompare.experience) return -1;
+            return 0;
+        }
+        public object Clone()
+        {
+            return new Engneer("Клон " + this.name, this.age, this.experience, this.qualification);
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
---
+++ Task14/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLibrary10;
+
+namespace Task14
+{
+    class Program
+    {
+        public static void EmployeeExp(List<List<Person>> list)
+        {
+            //Имена служащих со стажем не менее заданного
+            int exp = 40;
+            Console.WriteLine($"\nИмена служащих со стажем не менее заданного: {exp}\n");
+
+            var countByExp = from g in list
+                             from p in g
+                             where p is Employee && ((Employee)p).Experience >= exp
+                             select p.Name;
+
+            //var countByExp = list.SelectMany(g => g).Select(p => p).Where(p => p is Employee && ((Employee)p).Experience >= exp).OrderBy(p=>p.Name);
+
+            foreach (var item in countByExp)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+        public static void countOnPlant(List<List<Person>> list)
+        {
+            //Количество рабочих на заводе
+            string plant = "Пороховой завод";
+            Console.WriteLine($"\nКоличество рабочих на заводе: {plant}\n");
+
+            var countWorkers = (from g in list
+                                from p in g
+                                where p is Worker && ((Worker)p).WorkPlace == plant
+                                select p).Count();
+
+            //var countWorkers = list.SelectMany(g => g).Select(p => p).Where(p => p is Worker && ((Worker)p).WorkPlace == plant).Count();
+            Console.WriteLine($"\nКоличество рабочих на заводе {plant}:  {countWorkers}\n");
+        }
+        public static void Different(List<List<Person>> list)
+        {
+            //Разность множеств Employee||Worker||Engneer and Eployee
+            Console.WriteLine($"\nРазность множеств Employee||Worker||Engneer and Eployee\n");
+
+            var personDiff = (from g in list
+                              from p in g
+                                  //where p is Engneer
+                              select p).Except(from g2 in list
+                                               from p2 in g2
+                                               where p2 is Worker
+                                               select p2);
+
+            //var personDiff = list.SelectMany(g => g).Select(p => p).Except(list.SelectMany(g2 => g2).Select(p2 => p2).Where(p2 => p2 is Worker));
+            foreach (var item in personDiff)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+        public static void MiddleAge(List<List<Person>> list)
+        {
+            //Средний возраст рабочих
+            var middleAge = (from g in list
+                             from p in g
+                             where p is Worker
+                             select p.Age).Average();
+
+            //var middleAge = list.SelectMany(g => g).Select(p => p).Average(p => p.Age);
+            Console.WriteLine($"\nСредний возраст рабочих {Math.Round( middleAge,2)}\n");
+        }
+        public static void Groupping(List<List<Person>> list)
+        {
+            //Группировка рабочик по заводу
+            var groups = from g in list
+                         from p in g
+                         orderby p.Age
+                         group p by p.Age;
+            //var groups = list.SelectMany(g => g).Select(p => p).OrderBy(p => p.Age).GroupBy(p => p.Age);
+            foreach(IGrouping<int, Person> g in groups)
+            {
+                Console.WriteLine(g.Key);
+                    foreach (var e in g)
+                        Console.WriteLine(e);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Person> factory = new List<Person>();
+            List<Person> workshop = new List<Person>();
+
+            List<List<Person>> list = new List<List<Person>>();
+
+            int n = 6;
+            for(int i=0;i<n;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    factory.Add(new Employee());
+                    workshop.Add(new Employee());
+                }
+                if (i % 3 == 0)
+                {
+                    factory.Add(new Worker());
+                    workshop.Add(new Worker());
+                }
+                else
+                {
+                    factory.Add(new Engneer());
+                    workshop.Add(new Engneer());
+                }
+            }
+
+            foreach (Person item in factory)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine();
+            foreach (Person item in workshop)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            list.Add(factory);
+            list.Add(workshop);
+
+            //Имена служащих со стажем не менее заданного
+            EmployeeExp(list);
+
+            //Количество рабочих на заводе
+            countOnPlant(list);
+
+            //Разность множеств Engneer and Eployee
+            Different(list);
+
+            //Средний возраст рабочих
+            MiddleAge(list);
+
+            //Группировка рабочик по заводу
+            Groupping(list);
+
+            Console.ReadKey();
+        }
+    }
+}
